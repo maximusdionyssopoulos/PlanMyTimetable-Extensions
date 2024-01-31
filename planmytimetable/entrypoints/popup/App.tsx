75e88bab9117926d95857761668b68837023b22d
s3943811 <s3943811@student.rmit.edu.au>
2024-01-31 18:25:31 +1100

    Optimise component with solid Js built in components for and show

--- planmytimetable/entrypoints/popup/App.tsx
+++ planmytimetable/entrypoints/popup/App.tsx
@@ -1,40 +1,5 @@
-import { createEffect, createSignal, onMount, For } from "solid-js";
+import { createEffect, createSignal, onMount, For, Show } from "solid-js";
 import { ChevronDown, ChevronUp } from "lucide-solid";
-
-type ActivityGroup = {
-  subject_code: string;
-  activity_group_code: string;
-  description: string;
-  num_flagged_timeslots: string;
-  status: string;
-  auto_single: string;
-  min_prefs: number;
-  visible_acts_cnt?: number;
-  single_act: string;
-  allow_justification: string;
-  allow_waitlist: string;
-  show_availability: string;
-  act_cnt: number;
-};
-
-type Subject = {
-  subject_code: string;
-  description: string;
-  manager: string;
-  email_address: string;
-  faculty: string;
-  semester: string;
-  semester_description: string;
-  campus: string;
-  campus_description: string;
-  timezone: string;
-  showOnTT: string;
-  display_subject_code: string;
-  callista_code: string;
-  groups: Record<string, ActivityGroup>;
-};
-
-type Semester = Record<string, Subject>;
 
 type CourseStructure = Record<string, { semester: Semester; visible: boolean }>;
 
@@ -84,29 +49,32 @@ function App() {
   return (
     <>
       <div class=" w-[14rem] h-[17rem] p-2 overflow-y-hidden dark:bg-neutral-800 dark:text-white">
-        {Object.entries(count()).map(([semester_name, item]) => (
-          <>
-            <fieldset class="flex flex-row items-center border p-1 px-2 gap-2 rounded-md dark:border-neutral-600">
-              <input
-                type="checkbox"
-                name="semester"
-                value={semester_name}
-                id={`semester_${semester_name}`}
-              />
-              <label
-                for={`semester_${semester_name}`}
-                class=" font-medium text-sm"
-              >
-                {semester_name}
-              </label>
-              <button onClick={() => toggleVisiblity(semester_name)}>
-                <ChevronDown class=" size-4 hover:dark:bg-neutral-600 hover:bg-neutral-100 rounded-sm" />
-              </button>
-            </fieldset>
-
-            <ClassList semester={item.semester} />
-          </>
-        ))}
+        <For each={Object.entries(count())}>
+          {([semester_name, semester]) => (
+            <>
+              <fieldset class="flex flex-row items-center border p-1 px-2 gap-2 rounded-md dark:border-neutral-600">
+                <input
+                  type="checkbox"
+                  name="semester"
+                  value={semester_name}
+                  id={`semester_${semester_name}`}
+                />
+                <label
+                  for={`semester_${semester_name}`}
+                  class=" font-medium text-sm"
+                >
+                  {semester_name}
+                </label>
+                <button onClick={() => toggleVisiblity(semester_name)}>
+                  <ChevronDown class=" size-4 hover:dark:bg-neutral-600 hover:bg-neutral-100 rounded-sm" />
+                </button>
+              </fieldset>
+              <Show when={semester.visible}>
+                <ClassList semester={semester.semester} />
+              </Show>
+            </>
+          )}
+        </For>
       </div>
       <div class="p-2 overflow-y-hidden dark:bg-neutral-700 dark:text-white border-t dark:border-t-neutral-500">
         <h1 class="font-medium text-sm">PlanMyTimetable Capture</h1>
@@ -127,5 +95,40 @@ const ClassList = ({ semester }: classListProps) => (
     ))}
   </ul>
 );
+
+type ActivityGroup = {
+  subject_code: string;
+  activity_group_code: string;
+  description: string;
+  num_flagged_timeslots: string;
+  status: string;
+  auto_single: string;
+  min_prefs: number;
+  visible_acts_cnt?: number;
+  single_act: string;
+  allow_justification: string;
+  allow_waitlist: string;
+  show_availability: string;
+  act_cnt: number;
+};
+
+type Subject = {
+  subject_code: string;
+  description: string;
+  manager: string;
+  email_address: string;
+  faculty: string;
+  semester: string;
+  semester_description: string;
+  campus: string;
+  campus_description: string;
+  timezone: string;
+  showOnTT: string;
+  display_subject_code: string;
+  callista_code: string;
+  groups: Record<string, ActivityGroup>;
+};
+
+type Semester = Record<string, Subject>;
 
 export default App;
