40303e250a11689289e4ec3580188e411fde9346
s3943811 <s3943811@student.rmit.edu.au>
2024-01-31 23:02:27 +1100

    Add custom scrollbar styling

--- wxt/entrypoints/load_world.injected.ts
+++ wxt/entrypoints/load_world.injected.ts
@@ -1,18 +1,19 @@
 export default defineUnlistedScript({
   main() {
     let data;
+    const script = document.createElement("script");
     const handleFromWeb = async (event: MessageEvent) => {
       if (event.data.from === "msg.js") {
         data = event.data.data;
         browser.runtime.sendMessage(data);
+        script.remove();
       }
     };
 
     window.addEventListener("message", handleFromWeb);
 
-    // load script on
+    // load script when loaded
 
-    const script = document.createElement("script");
     script.src = browser.runtime.getURL("/load.js");
     document.body.appendChild(script);
   },
