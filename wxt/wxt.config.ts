6f27c22a387c39c6e3498aa5e9fcb8dd408acd1f
s3943811 <s3943811@student.rmit.edu.au>
2024-02-02 00:16:16 +1100

    Update wxt.config.ts with new manifest settings

--- wxt/wxt.config.ts
+++ wxt/wxt.config.ts
@@ -11,10 +11,33 @@ export default defineConfig({
   }),
   runner: {
     disabled: true,
-    chromiumProfile:
-      "/Users/maximusdionyss/Library/Application Support/Google/Chrome/Default",
   },
   manifest: {
-    permissions: ["activeTab", "scripting"],
+    name: "PlanMyTimetable Capture",
+    description: "Capture timetable data to use on PlanMyTimetable.",
+    browser_specific_settings: {
+      gecko: {
+        id: "Capture@PlanMyTimetable.org",
+        strict_min_version: "109.0",
+      },
+    },
+    permissions: ["activeTab", "scripting", "tabs"],
+    browser_action: {
+      default_title: "PlanMyTimetable Capture",
+      default_icon: {
+        "16": "icon-greyed.svg",
+        "48": "icon-greyed.svg",
+        "96": "icon-greyed.svg",
+        "128": "icon-greyed.svg",
+        "256": "icon-greyed.svg",
+      },
+    },
+    icons: {
+      "16": "icon.svg",
+      "48": "icon.svg",
+      "96": "icon.svg",
+      "128": "icon.svg",
+      "256": "icon.svg",
+    },
   },
 });
