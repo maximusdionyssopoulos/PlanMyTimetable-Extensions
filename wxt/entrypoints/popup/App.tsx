b69863792c725d54502d1bb227a62e89911e7ba4
s3943811 <s3943811@student.rmit.edu.au>
2024-02-02 00:17:06 +1100

    Remove unused entrypoints and refactor popup to inject scripts using exceute script and work like chrome extension

--- wxt/entrypoints/popup/App.tsx
+++ wxt/entrypoints/popup/App.tsx
@@ -1,31 +1,18 @@
-import { createEffect, createSignal, onMount, For, Show } from "solid-js";
-import { ChevronDown, ChevronUp } from "lucide-solid";
+import {
+  createSignal,
+  onMount,
+  For,
+  Show,
+  type Accessor,
+  type Setter,
+} from "solid-js";
+import { ChevronDown, ChevronUp, AlertTriangleIcon } from "lucide-solid";
+import JSONCrush from "jsoncrush";
 
 type CourseStructure = Record<string, { semester: Semester; visible: boolean }>;
-
+type CaptureState = "IDLE" | "CAPTURING" | "ERROR" | "LOADING";
 function App() {
-  const [count, setCount] = createSignal<CourseStructure>({});
-  const [classes, setClasses] = createSignal<Subject[]>([]);
-
-  const getCurrentTab = async () => {
-    const [tab] = await browser.tabs.query({
-      active: true,
-      lastFocusedWindow: true,
-    });
-    return tab;
-  };
-
-  const toggleVisiblity = (semester_name: string) => {
-    const course = count();
-    setCount(() => ({
-      ...course,
-      [semester_name]: {
-        ...course[semester_name],
-        visible: !course[semester_name].visible,
-      },
-    }));
-  };
-
+  //////////////////////////////////////////////////////////////////////////////////////////////////
   /**
    * This function runs inside the "world" of the page, meaning it has access to variables
    * this fetches data, creates objects before returning them
@@ -85,7 +72,6 @@ function App() {
           return 4;
       }
     };
-
     const data = await Promise.all(
       classes.map(async (course, index) => {
         return Promise.all(
@@ -93,10 +79,10 @@ function App() {
             console.log(
               `Downloading group ${course.description} - ${group.activity_group_code}`
             );
-            const url = new URL(window.wrappedJSObject.location.href);
+            const url = new URL(window.location.href);
             const token = url.searchParams.get("ss");
 
-            const path_base = window.wrappedJSObject.location.pathname
+            const path_base = window.location.pathname
               .split("/")
               .slice(0, -1)
               .join("/");
@@ -104,6 +90,7 @@ function App() {
             // create url to fetch data from
             const fetchUrl = new URL(
               `${url.origin}${path_base}/rest/student/${
+                // @ts-ignore
                 window.wrappedJSObject.data.student.student_code
               }/subject/${course.subject_code}/group/${
                 group.activity_group_code
@@ -147,99 +134,238 @@ function App() {
         );
       })
     );
-    console.log(data);
-
-    // window.postMessage({
-    //   from: "s.js",
-    //   data: data,
-    // });
+    return data.flat();
   };
 
-  browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (sender.id === browser.runtime.id) {
-      const course = Object.entries(message).map(([name, item]) => [
-        name,
-        { semester: item, visible: false },
-      ]);
-      setCount(Object.fromEntries(course));
-      setClasses(Object.values(message["HE Sem 01"]) as Subject[]);
-    }
-  });
+  const getStudentData = () => {
+    // @ts-ignore
+    return window.wrappedJSObject.data.student.student_enrolment_sem;
+  };
 
-  // createEffect(() => {
-  //   console.log(count());
-  // });
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  const [course, setCourse] = createSignal<CourseStructure>({});
+  const [classes, setClasses] = createSignal<Subject[]>([]);
+  const [captureState, setCaptureState] = createSignal<CaptureState>("LOADING");
+
+  const getCurrentTab = async () => {
+    const [tab] = await browser.tabs.query({
+      active: true,
+      lastFocusedWindow: true,
+    });
+    return tab;
+  };
+
+  const toggleVisiblity = (semester_name: string) => {
+    setCourse((course) => ({
+      ...course,
+      [semester_name]: {
+        ...course[semester_name],
+        visible: !course[semester_name].visible,
+      },
+    }));
+  };
+  /**
+   * This method is to check whether a semester is selected by checing whether every element in semester is in classes
+   * @param semester
+   * @returns
+   */
+  const isSemSelected = (semester: Semester) => {
+    return Object.values(semester).every((item) => classes().includes(item));
+  };
+
+  /**
+   * This function will update the classes array when the semester checkbox is ticked/unticked
+   * if the semester is already selected, then remove the classes from that semester when unchecking
+   * else if the semester isn't selected then add the classes in that semester
+   * we use a set to ensure that the classes array only has unique values
+   * @param semester
+   */
+  const handleSemesterChange = (semester: Semester) => {
+    if (isSemSelected(semester)) {
+      setClasses((classes) =>
+        classes.filter((item) => !Object.values(semester).includes(item))
+      );
+    } else {
+      setClasses((classes) => [
+        ...new Set([...classes, ...Object.values(semester)]),
+      ]);
+    }
+  };
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////
 
   onMount(async () => {
-    await browser.scripting.executeScript({
+    const [{ result }] = await browser.scripting.executeScript({
       target: { tabId: (await getCurrentTab()).id ?? 0 },
-      files: ["/load_world.js"],
+      func: getStudentData,
     });
+
+    /**
+     * This is the data returned from the script, it is the student data...
+     * as a result we can set our course and default classes
+     */
+
+    const course = Object.entries(result).map(([name, item], index) => [
+      name,
+      { semester: item as Semester, visible: index === 0 },
+    ]);
+    setCourse(Object.fromEntries(course));
+
+    if (typeof course[0][1] === "object") {
+      setClasses(Object.values(course[0][1].semester));
+    }
+    setCaptureState("IDLE");
   });
 
   return (
-    <div class="group dark:bg-neutral-800 dark:text-white w-[14rem] flex flex-col  ">
-      <div class="space-y-2 p-2 h-[17rem] overflow-y-scroll scrollbar group-hover:[&::-webkit-scrollbar-thumb]:bg-neutral-500/80">
-        <For each={Object.entries(count())}>
+    <div class="group dark:bg-neutral-800 dark:text-white w-[14rem] flex flex-col ">
+      <Show when={captureState() === "CAPTURING"}>
+        <div class="flex items-center justify-center w-full z-10 h-[17rem] absolute text-sm text-center font-medium px-2">
+          Capturing your timetable data...
+        </div>
+      </Show>
+      <Show when={captureState() === "ERROR"}>
+        <div class="flex flex-col items-center justify-center w-full z-10 h-[17rem] absolute text-sm text-center font-medium px-2 bg-white dark:bg-neutral-800 gap-1">
+          <div class=" rounded-3xl  p-4 dark:border-neutral-600 border">
+            <AlertTriangleIcon class=" size-8 stroke-red-600 dark:stroke-red-400" />
+          </div>
+          <h1 class="text-base font-medium">Uh oh, something went wrong</h1>
+          <div class="space-y-2">
+            <p class="text-sm">
+              Please check your internet connection, refresh the page and try
+              again.
+            </p>
+            <p class="text-xs">
+              If the problem persists, please open a new issue&nbsp;
+              <a
+                href="https://github.com/s3943811/timetable/issues/new"
+                class="font-medium underline underline-offset-4 after:content-['_↗']"
+              >
+                on Github
+              </a>
+            </p>
+          </div>
+        </div>
+      </Show>
+      <div
+        class={`space-y-2 p-2 h-[17rem] overflow-y-scroll scrollbar group-hover:[&::-webkit-scrollbar-thumb]:bg-neutral-500/80 ${
+          captureState() === "CAPTURING" ? "opacity-5" : ""
+        }`}
+      >
+        <For each={Object.entries(course())}>
           {([semester_name, semester]) => (
-            <>
-              <div class="flex flex-row border p-1 px-2 rounded-md dark:border-neutral-600 justify-between">
-                <fieldset class="flex flex-row items-center gap-2">
-                  <input
-                    type="checkbox"
-                    name="semester"
-                    value={semester_name}
-                    id={`semester_${semester_name}`}
-                  />
-                  <label
-                    for={`semester_${semester_name}`}
-                    class=" font-medium text-sm"
+            <div class="border-b last:border-b-0 dark:border-neutral-600 pb-2">
+              <div class="flex flex-row  p-1 px-2 justify-between ">
+                <div class="flex flex-row items-center gap-2">
+                  <button
+                    class="inline-flex gap-1 items-center font-medium text-sm"
+                    onClick={() => toggleVisiblity(semester_name)}
                   >
+                    <Show
+                      when={semester.visible}
+                      fallback={<ChevronUp class=" size-4" />}
+                    >
+                      <ChevronDown class=" size-4" />
+                    </Show>
                     {semester_name}
-                  </label>
-                </fieldset>
-                <button onClick={() => toggleVisiblity(semester_name)}>
-                  <ChevronDown class=" size-4 " />
-                </button>
+                  </button>
+                </div>
+                <input
+                  type="checkbox"
+                  name="semester"
+                  value={semester_name}
+                  id={`semester_${semester_name}`}
+                  checked={isSemSelected(semester.semester)}
+                  onChange={() => handleSemesterChange(semester.semester)}
+                />
               </div>
               <Show when={semester.visible}>
-                <ClassList semester={semester.semester} />
+                <ClassList
+                  semester={semester.semester}
+                  classes={classes}
+                  setClasses={setClasses}
+                />
               </Show>
-            </>
+            </div>
           )}
         </For>
       </div>
       <button
-        class="w-24 m-0.5 place-self-center min-h-7  bg-yellow-400 text-yellow-800 px-2 rounded-md hover:bg-yellow-500"
+        class="w-24 m-0.5 place-self-center min-h-7  bg-yellow-400 text-yellow-800 px-2 rounded-md hover:bg-yellow-500 disabled:opacity-50 disabled:cursor-not-allowed
+        "
+        disabled={captureState() === "CAPTURING"}
         onClick={async () => {
-          await browser.scripting.executeScript({
+          setCaptureState("CAPTURING");
+          const [{ result }] = await browser.scripting.executeScript({
             target: { tabId: (await getCurrentTab()).id ?? 0 },
             func: capture,
             args: [classes()],
           });
+
+          if (!result) {
+            setCaptureState("ERROR");
+            return;
+          }
+
+          /**
+           * This data will be returned from the capture function, we then encoded and open a new window
+           */
+          const encoded = encodeURIComponent(
+            JSONCrush.crush(JSON.stringify(result))
+          );
+
+          window
+            .open(`https://planmytimetable.vercel.app/?state=${encoded}`)
+            ?.focus();
+          window.close();
         }}
       >
         Capture
       </button>
-      <div class="p-2 overflow-y-hidden dark:bg-neutral-700 dark:text-white border-t dark:border-t-neutral-500">
-        <h1 class="font-medium text-sm">PlanMyTimetable Capture</h1>
-      </div>
     </div>
   );
 }
 
 interface classListProps {
   semester: Semester;
+  classes: Accessor<Subject[]>;
+  setClasses: Setter<Subject[]>;
 }
-const ClassList = ({ semester }: classListProps) => (
-  <ul class="list-disc list-inside px-2">
-    {Object.values(semester).map((class_item) => (
-      <li class="text-xs">
-        {class_item.description} - {class_item.callista_code}
-      </li>
-    ))}
-  </ul>
-);
+const ClassList = ({ semester, classes, setClasses }: classListProps) => {
+  /**
+   * method to remove and add item from classes array
+   * if already in array find index and remove
+   * else add it to classes array
+   * @param item - the item to remove/add
+   */
+  const handleClassesUpdate = (item: Subject) => {
+    if (classes().includes(item)) {
+      const index = classes().findIndex((value) => value === item);
+      setClasses((classes) => classes.toSpliced(index, 1));
+    } else {
+      setClasses((classes) => [...classes, item]);
+    }
+  };
+  return (
+    <ul class=" list-none list-inside px-2">
+      <For each={Object.values(semester)}>
+        {(item) => (
+          <li class="text-xs inline-flex gap-2">
+            <input
+              type="checkbox"
+              name="semester"
+              value={item.callista_code}
+              id={`class_${item.callista_code}`}
+              checked={classes().includes(item)}
+              onChange={() => handleClassesUpdate(item)}
+            />
+            {item.description} ({item.callista_code})
+          </li>
+        )}
+      </For>
+    </ul>
+  );
+};
 
 type ActivityGroup = {
   subject_code: string;
